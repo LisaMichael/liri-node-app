5c53c1d7ea1b1ebdae8947713859d8c5389acd17
Lisa <lisa.michael@hotmail.com>
2019-11-24 14:31:52 -0500

    event date time is now formattign correctly using moment

--- liri.js
+++ liri.js
@@ -1,17 +1,27 @@
-// use dotenv
+// use dotenv package takes what is in your .env file and puts it in process.env
 require("dotenv").config();
 
 
 // declare npm packages 
-let axios = require("axios");
-let keys = require("./keys.js");
-let fs = require("fs");
-let Spotify = require("node-spotify-api");
-let nodeArgs = process.argv;
 
+//Axios is used to send asynchronous HTTP requests to REST endpoints and perform CRUD operations
+let axios = require("axios");
+//code required to import the `keys.js` file and store it in a variable.
+let keys = require("./keys.js");
+//fs module allows you to work with the file system on your computer for actions such as read/write.
+let fs = require("fs");
+//npm used for spotify api
+let Spotify = require("node-spotify-api");
+
+//declaration of user input, which is placed in 3rd element of process.argv array
+//splice() method adds/removes items to/from the array
 let userInput = process.argv.splice(3).join(" ");
+//declare the task later used in switch code
 let task = process.argv[2];
 // create new spotify object
+
+// moment npm allows formatting of date time
+let moment = require("moment");
 let spotify = new Spotify(keys.spotify);
 
 // switch statement where i am calling different functions which call the apis used
@@ -43,13 +53,21 @@ function bands() {
   axios.get(bandQuery).then(
     function (response) {
       // console.log(response);
+     
+
       let venuesArray = response.data;
       console.log("Venue: ", response.data[0].venue.name);
       //console.log("Venue: ", JSON.stringify(response.data));
       let temp = response.data[0].datetime.split("T");
 
+      //sent variable for datetime , and use moment to change format
+      var eventDateTime = moment(response.data[0].datetime);
+
       console.log("Venue location: " + response.data[0].venue.city);
-      console.log("Venue location: " + temp[0]);
+      
+// display date time in MM/DD/YYYY format
+      console.log("Event Date & Time: " + eventDateTime.format("MM/DD/YYYY"));
+
     })
     .catch(function (error) {
       if (error.response) {
@@ -79,6 +97,8 @@ function bands() {
 function song() {
 
   // this code was taken from example listed on https://www.npmjs.com/package/node-spotify-api
+  
+  //if the user input for spotify-this-song is blank, default to Ace of Base , "The Sign"
   if (userInput === "") {
     userInput = "The Sign ace of base";
     spotify
@@ -96,6 +116,8 @@ function song() {
   })
   }
 else{
+
+  // Display the first 5 responses for song entered in "Spotify-this-song"
   spotify
     .search({ type: 'track', query: userInput, limit: 5 })
     .then(function (response) {
@@ -121,7 +143,6 @@ else{
 
 // OMDBAPI code should go here for "movie-this" code "
 // call movie-this code here 
-
 function movieQuery() {
 
   // if userput for movie-this is blank, default to Mr. Nobody.
